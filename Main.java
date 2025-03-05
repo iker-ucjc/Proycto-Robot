83360bd52eebc75a410f329f5f4d9e66e06475e1
iker-ucjc <iker.rodriguez@alumno.ucjc.edu>
2025-03-05 19:51:07 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] argumentos) {
         Robot miRobot = new Robot("robot", 100, 100);
         Scanner scanner = new Scanner(System.in);
 
+        OUTER:
         while (true) {
             System.out.println("\n=== MENÚ ===");
             System.out.println("1 - Mostrar información del robot");
@@ -15,52 +17,45 @@ public class Main {
             System.out.println("5 - Salir");
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir la nueva línea
-
-            if (opcion == 1) {
-                miRobot.mostrarInfo();
-            } 
-            // Función de juego
-            else if (opcion == 2) {
-                Mapa mapa = new Mapa(10, 10);
-                Scanner input = new Scanner(System.in);
-                
-                while (mapa.i != mapa.largo - 1 && mapa.j != mapa.ancho - 1) { // Bucle hasta llegar a (filas, columnas)
-                    System.out.print("¿Desea iniciar movimiento? (Y/N): ");
-                    String respuesta = input.nextLine();
-                    
-                    if (respuesta.equalsIgnoreCase("Y")) {
-                        mapa.imprimirMapa();
-                        Movimiento movimiento = new Movimiento(mapa);
-                        movimiento.mover();
-                        movimiento.imprimirposicion();
-                        mapa.zombie();
-                        mapa.salud();
-                        mapa.carga();
-                        mapa.imprimirMapa();
-                    } else if (respuesta.equalsIgnoreCase("N")) {
-                        System.out.println("Saliendo del juego....");
-                        break;
-                    } else {
-                        System.out.println("Ingrese Y si quiere iniciar el movimiento o N para salir");
-                    }
-                }
-            } 
-            // Función para aumentar la energía
-            else if (opcion == 3) {
-                Robot.recargar();
-            } 
-            // Función para aumentar la vida
-            else if (opcion == 4) {
-                Robot.reparar();
-            } 
-            // Función para salir del menú del juego
-            else if (opcion == 5) {
-                System.out.println("Saliendo...");
-                break;
-            } 
-            else {
-                System.out.println("Comando no válido, intente de nuevo.");
+            scanner.nextLine();
+            switch (opcion) {
+                case 1:
+                    miRobot.mostrarInfo();
+                    break;
+                case 2:
+                    Mapa mapa = new Mapa(10, 10);
+                    while (mapa.i != mapa.largo - 1 && mapa.j != mapa.ancho - 1) { 
+                        System.out.print("¿Desea iniciar movimiento? (Y/N): ");
+                        String respuesta = scanner.nextLine();
+                        
+                        if (respuesta.equalsIgnoreCase("Y")) {
+                            mapa.imprimirMapa();
+                            Movimiento movimiento = new Movimiento(mapa);
+                            movimiento.mover();
+                            movimiento.imprimirposicion();
+                            mapa.zombie();
+                            mapa.salud();
+                            mapa.carga();
+                            mapa.imprimirMapa();
+                        } else if (respuesta.equalsIgnoreCase("N")) {
+                            System.out.println("Saliendo del juego....");
+                            break;
+                        } else {
+                            System.out.println("Ingrese Y si quiere iniciar el movimiento o N para salir");
+                        }
+                    }   break;
+                case 3:
+                    miRobot.recargar();
+                    break;
+                case 4:
+                    miRobot.reparar();
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    break OUTER;
+                default:
+                    System.out.println("Comando no válido, intente de nuevo.");
+                    break;
             }
         }   
         scanner.close();
